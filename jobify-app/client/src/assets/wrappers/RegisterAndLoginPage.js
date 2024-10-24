229f5747df0b1cdec6e5fe7a4428079f8263dafc
Adriano Ayala <adriano.ayc@gmail.com>
2024-10-24 12:21:25 -0400

    Login and Register CSS (optional)

--- jobify-app/client/src/assets/wrappers/RegisterAndLoginPage.js
+++ jobify-app/client/src/assets/wrappers/RegisterAndLoginPage.js
@@ -4,25 +4,25 @@ const Wrapper = styled.section`
   min-height: 100vh;
   display: grid;
   align-items: center;
-  .logo {
-    display: block;
-    margin: 0 auto;
-    margin-bottom: 1.38rem;
-  }
   .form {
     max-width: 400px;
     border-top: 5px solid var(--primary-500);
+  }
+  .logo {
+    margin: 0 auto;
+    margin-bottom: 1.38rem;
+    display: block;
   }
   h4 {
     text-align: center;
     margin-bottom: 1.38rem;
   }
-  p {
+  .btn {
     margin-top: 1rem;
+  }
+  p {
     text-align: center;
     line-height: 1.5;
-  }
-  .btn {
     margin-top: 1rem;
   }
   .member-btn {
@@ -31,4 +31,5 @@ const Wrapper = styled.section`
     margin-left: 0.25rem;
   }
 `;
+
 export default Wrapper;
