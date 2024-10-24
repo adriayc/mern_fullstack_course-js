e5b301c80b80683c8cb016c36510058acf01249b
Adriano Ayala <adriano.ayc@gmail.com>
2024-10-23 23:47:21 -0400

    Styled Components - Intro

--- jobify-app/client/src/pages/Landing.jsx
+++ jobify-app/client/src/pages/Landing.jsx
@@ -1,5 +1,18 @@
+import styled from 'styled-components';
+
+const StyledBtn = styled.button`
+  color: white;
+  background: red;
+  font-size: 1.5rem;
+`;
+
 const Landing = () => {
-  return <h1>Landing Page</h1>;
+  return (
+    <div>
+      <h1>Landing Page</h1>
+      <StyledBtn>Styled Btn</StyledBtn>
+    </div>
+  );
 };
 
 export default Landing;
