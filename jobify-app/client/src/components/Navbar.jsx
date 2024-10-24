3a6ea727e618189bb2ffa642a331ad94acff2148
Adriano Ayala <adriano.ayc@gmail.com>
2024-10-24 18:11:33 -0400

    Navbar Structure

--- jobify-app/client/src/components/Navbar.jsx
+++ jobify-app/client/src/components/Navbar.jsx
@@ -1,12 +1,25 @@
-import { FaHome } from 'react-icons/fa';
+import { FaAlignLeft } from 'react-icons/fa';
 // Wrapper (Styled Components)
 import Wrapper from '../assets/wrappers/Navbar';
+// Components
+import { Logo } from '../components';
+import { useDashboardContext } from '../pages/DashboardLayout';
 
 const Navbar = () => {
+  const { toggleSidebar } = useDashboardContext();
+
   return (
     <Wrapper>
-      <h2>Navbar</h2>
-      <FaHome />
+      <div className="nav-center">
+        <button type="button" className="toggle-btn" onClick={toggleSidebar}>
+          <FaAlignLeft />
+        </button>
+        <div>
+          <Logo />
+          <h4 className="logo-text">Dashboard</h4>
+        </div>
+        <div className="btn-container">Toggle/Logout</div>
+      </div>
     </Wrapper>
   );
 };
