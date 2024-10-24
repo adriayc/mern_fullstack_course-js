396299bfb8f1f906e8637e94e5bd6708e83545a4
Adriano Ayala <adriano.ayc@gmail.com>
2024-10-24 11:36:47 -0400

    Error Page - CSS (optional)

--- jobify-app/client/src/assets/wrappers/ErrorPage.js
+++ jobify-app/client/src/assets/wrappers/ErrorPage.js
@@ -4,23 +4,23 @@ const Wrapper = styled.main`
   min-height: 100vh;
   text-align: center;
   display: flex;
-  align-items: center;
   justify-content: center;
+  align-items: center;
   img {
     width: 90vw;
     max-width: 600px;
-    display: block;
-    margin-bottom: 2rem;
     margin-top: -3rem;
+    margin-bottom: 2rem;
+    display: block;
   }
   h3 {
     margin-bottom: 0.5rem;
   }
   p {
+    color: var(--text-secondary-color);
     line-height: 1.5;
     margin-top: 0.5rem;
     margin-bottom: 1rem;
-    color: var(--text-secondary-color);
   }
   a {
     color: var(--primary-500);
