d80c99ec774910b629cb487e7b56e020c7176ed3
Adriano Ayala <adriano.ayc@gmail.com>
2024-10-23 23:30:18 -0400

    Error Page

--- jobify-app/client/src/pages/Error.jsx
+++ jobify-app/client/src/pages/Error.jsx
@@ -1,5 +1,16 @@
+import { Link, useRouteError } from 'react-router-dom';
+
 const Error = () => {
-  return <h1>Error Page</h1>;
+  // This hook return anything thrown during an action, loader, or rendering
+  const error = useRouteError();
+  console.log(error);
+
+  return (
+    <div>
+      <h1>Error Page</h1>
+      <Link to="/">Back home</Link>
+    </div>
+  );
 };
 
 export default Error;
