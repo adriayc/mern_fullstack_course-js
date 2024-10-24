1f2ef09318ed3bc2f6e328c11e0f283e9df38f71
Adriano Ayala <adriano.ayc@gmail.com>
2024-10-24 11:36:03 -0400

    Error Page - JSX

--- jobify-app/client/src/pages/Error.jsx
+++ jobify-app/client/src/pages/Error.jsx
@@ -1,15 +1,31 @@
 import { Link, useRouteError } from 'react-router-dom';
+// Wrapper (Styled Components)
+import Wrapper from '../assets/wrappers/ErrorPage';
+// Images
+import img from '../assets/images/not-found.svg';
 
 const Error = () => {
   // This hook return anything thrown during an action, loader, or rendering
   const error = useRouteError();
-  console.log(error);
 
+  if (error.status === 404) {
+    return (
+      <Wrapper>
+        <div>
+          <img src={img} alt="Not found" />
+          <h3>Ohh! page not found</h3>
+          <p>We can't seem to find the page you are looking for</p>
+          <Link to="/dashboard">Back home</Link>
+        </div>
+      </Wrapper>
+    );
+  }
   return (
-    <div>
-      <h1>Error Page</h1>
-      <Link to="/">Back home</Link>
-    </div>
+    <Wrapper>
+      <div>
+        <h3>Something went wrong</h3>
+      </div>
+    </Wrapper>
   );
 };
 
