950cca1e1fead74417202c463d63ad997f8c7acd
Adriano Ayala <adriano.ayc@gmail.com>
2024-10-23 23:12:27 -0400

    Link Component

--- jobify-app/client/src/pages/Login.jsx
+++ jobify-app/client/src/pages/Login.jsx
@@ -1,5 +1,13 @@
+import { Link } from 'react-router-dom';
+
 const Login = () => {
-  return <h1>Login Page</h1>;
+  return (
+    <div>
+      <h1>Login Page</h1>
+      <Link to="/register">Register Page</Link>
+      <a href="https://johnsmilga.com/">Best web dev courses</a>
+    </div>
+  );
 };
 
 export default Login;
