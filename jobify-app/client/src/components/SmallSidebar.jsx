4a9866834ba4968882913f7582360347723acf44
Adriano Ayala <adriano.ayc@gmail.com>
2024-10-24 17:59:08 -0400

    Dashboard Context

--- jobify-app/client/src/components/SmallSidebar.jsx
+++ jobify-app/client/src/components/SmallSidebar.jsx
@@ -1,7 +1,12 @@
 // Wrapper (Styled Components)
 import Wrapper from '../assets/wrappers/SmallSidebar';
+// Custom hook (Context)
+import { useDashboardContext } from '../pages/DashboardLayout';
 
 const SmallSidebar = () => {
+  const data = useDashboardContext();
+  console.log(data);
+
   return <Wrapper>SmallSidebar</Wrapper>;
 };
 
--- jobify-app/client/src/pages/DashboardLayout.jsx
+++ jobify-app/client/src/pages/DashboardLayout.jsx
@@ -1,24 +1,60 @@
+import { createContext, useContext, useState } from 'react';
 import { Outlet } from 'react-router-dom';
 // Wrapper (Styled Components)
 import Wrapper from '../assets/wrappers/Dashboard';
 // Components
 import { SmallSidebar, BigSidebar, Navbar } from '../components';
 
+// Define context
+const DashboardContext = createContext();
+
 const DashboardLayout = () => {
+  // Temp
+  const user = { name: 'adriano' };
+
+  const [showSidebar, setShowSidebar] = useState(false);
+  const [isDarkTheme, setIsDarkTheme] = useState(false);
+
+  const toggleDarkTheme = () => {
+    console.log('Toggle dark theme');
+  };
+
+  const toggleSidebar = () => {
+    setShowSidebar(!showSidebar);
+  };
+
+  const logoutUser = async () => {
+    console.log('Logout user');
+  };
+
   return (
-    <Wrapper>
-      <main className="dashboard">
-        <SmallSidebar />
-        <BigSidebar />
-        <div>
-          <Navbar />
-          <div className="dashboard-page">
-            <Outlet />
+    <DashboardContext.Provider
+      value={{
+        user,
+        showSidebar,
+        isDarkTheme,
+        toggleDarkTheme,
+        toggleSidebar,
+        logoutUser,
+      }}
+    >
+      <Wrapper>
+        <main className="dashboard">
+          <SmallSidebar />
+          <BigSidebar />
+          <div>
+            <Navbar />
+            <div className="dashboard-page">
+              <Outlet />
+            </div>
           </div>
-        </div>
-      </main>
-    </Wrapper>
+        </main>
+      </Wrapper>
+    </DashboardContext.Provider>
   );
 };
 
+// Export custom hook (Context)
+export const useDashboardContext = () => useContext(DashboardContext);
+// Export component
 export default DashboardLayout;
