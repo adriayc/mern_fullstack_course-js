bfb6833768850a19093fb35d5395478061e8dfbe
Adriano Ayala <adriano.ayc@gmail.com>
2024-10-24 23:32:32 -0400

    Sidebar Structure

--- jobify-app/client/src/components/SmallSidebar.jsx
+++ jobify-app/client/src/components/SmallSidebar.jsx
@@ -1,13 +1,42 @@
+import { NavLink } from 'react-router-dom';
+import { FaTimes } from 'react-icons/fa';
 // Wrapper (Styled Components)
 import Wrapper from '../assets/wrappers/SmallSidebar';
 // Custom hook (Context)
 import { useDashboardContext } from '../pages/DashboardLayout';
+// Utils
+import links from '../utils/links';
+// Components
+import { Logo } from '../components';
 
 const SmallSidebar = () => {
   const data = useDashboardContext();
-  console.log(data);
 
-  return <Wrapper>SmallSidebar</Wrapper>;
+  return (
+    <Wrapper>
+      <div className="sidebar-container show-sidebar">
+        <div className="content">
+          <button type="button" className="close-btn">
+            <FaTimes />
+          </button>
+          <header>
+            <Logo />
+          </header>
+          <div className="nav-links">
+            {links.map((link) => {
+              const { text, path, icon } = link;
+              return (
+                <NavLink key={text} to={path} className="nav-link">
+                  <span className="icon">{icon}</span>
+                  {text}
+                </NavLink>
+              );
+            })}
+          </div>
+        </div>
+      </div>
+    </Wrapper>
+  );
 };
 
 export default SmallSidebar;
