2cffc139b0468829044ee2ce6eb6e9c88a4e1907
Adriano Ayala <adriano.ayc@gmail.com>
2024-10-25 00:16:21 -0400

    NavLinks Component

--- jobify-app/client/src/components/SmallSidebar.jsx
+++ jobify-app/client/src/components/SmallSidebar.jsx
@@ -1,13 +1,11 @@
-import { NavLink } from 'react-router-dom';
 import { FaTimes } from 'react-icons/fa';
 // Wrapper (Styled Components)
 import Wrapper from '../assets/wrappers/SmallSidebar';
 // Custom hook (Context)
 import { useDashboardContext } from '../pages/DashboardLayout';
-// Utils
-import links from '../utils/links';
 // Components
 import { Logo } from '../components';
+import NavLinks from './NavLinks';
 
 const SmallSidebar = () => {
   const { showSidebar, toggleSidebar } = useDashboardContext();
@@ -26,24 +24,7 @@ const SmallSidebar = () => {
           <header>
             <Logo />
           </header>
-          <div className="nav-links">
-            {links.map((link) => {
-              const { text, path, icon } = link;
-              return (
-                <NavLink
-                  key={text}
-                  to={path}
-                  className="nav-link"
-                  onClick={toggleSidebar}
-                  // The end prop changes the matching logic for the active and pending states to only match to the "end" of the NavLink's to path. If the URL is longer than to, it will no longer be considered active.
-                  end
-                >
-                  <span className="icon">{icon}</span>
-                  {text}
-                </NavLink>
-              );
-            })}
-          </div>
+          <NavLinks />
         </div>
       </div>
     </Wrapper>
