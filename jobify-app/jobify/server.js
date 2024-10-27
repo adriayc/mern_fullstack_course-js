9ae10b151a2aae630b0c92eab4d6b7e763d1c442
Adriano Ayala <adriano.ayc@gmail.com>
2024-10-27 00:57:32 -0400

    Get Single Job

--- jobify-app/jobify/server.js
+++ jobify-app/jobify/server.js
@@ -46,6 +46,17 @@ app.post('/api/v1/jobs', (req, res) => {
 
   res.status(200).json({ job });
 });
+// Get Single Job
+app.get('/api/v1/jobs/:id', (req, res) => {
+  const { id } = req.params;
+
+  const job = jobs.find((job) => job.id === id);
+  if (!job) {
+    return res.status(404).json({ msg: `no job with id: ${id} exist` });
+  }
+
+  res.status(200).json({ job });
+});
 
 const port = process.env.PORT || 5100;
 app.listen(port, () => {
