4ae82907436b77321071ea1dbbe5ade081923c20
Adriano Ayala <adriano.ayc@gmail.com>
2024-10-27 00:58:17 -0400

    Delete Job

--- jobify-app/jobify/server.js
+++ jobify-app/jobify/server.js
@@ -76,6 +76,21 @@ app.patch('/api/v1/jobs/:id', (req, res) => {
 
   res.status(200).json({ job });
 });
+// Delete Job
+app.delete('/api/v1/jobs/:id', (req, res) => {
+  const { id } = req.params;
+
+  const job = jobs.find((job) => job.id === id);
+  if (!job) {
+    return res.status(404).json({ msg: `no job with id: ${id} exist` });
+  }
+
+  const newJobs = jobs.filter((job) => job.id !== id);
+
+  jobs = newJobs;
+
+  res.status(200).json({ msg: 'job deleted' });
+});
 
 const port = process.env.PORT || 5100;
 app.listen(port, () => {
