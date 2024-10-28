2bd66472be89472385b7016387caa9138aaf5e9a
Adriano Ayala <adriano.ayc@gmail.com>
2024-10-28 00:14:07 -0400

    Create Job Controller

--- jobify-app/jobify/controllers/jobControllers.js
+++ jobify-app/jobify/controllers/jobControllers.js
@@ -1,4 +1,6 @@
 import { nanoid } from 'nanoid';
+// Models
+import Job from '../models/JobModel.js';
 
 let jobs = [
   { id: nanoid(), company: 'apple', position: 'front-end' },
@@ -12,19 +14,8 @@ export const getAllJobs = async (req, res) => {
 
 // Create Job
 export const createJob = async (req, res) => {
-  const { company, position } = req.body;
-
-  if (!company || !position) {
-    // return res.status(400).json({ msg: 'please provide company and position' });
-    res.status(400).json({ msg: 'please provide company and position' });
-    return;
-  }
-
-  const id = nanoid();
-  const job = { id, company, position };
-
-  jobs.push(job);
-
+  const job = await Job.create(req.body);
+  // const job = await Job.create('something'); // Error
   res.status(200).json({ job });
 };
 
