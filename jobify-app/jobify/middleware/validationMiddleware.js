873c88cc513866bd6b2a5e66377c15c99bfdad24
Adriano Ayala <adriano.ayc@gmail.com>
2024-10-30 18:49:58 -0400

    Validate Owner

--- jobify-app/jobify/middleware/validationMiddleware.js
+++ jobify-app/jobify/middleware/validationMiddleware.js
@@ -4,7 +4,11 @@ import { body, param, validationResult } from 'express-validator';
 import Job from '../models/JobModel.js';
 import User from '../models/UserModel.js';
 // Errors
-import { BadRequestError, NotFoundError } from '../errors/customErrors.js';
+import {
+  BadRequestError,
+  NotFoundError,
+  UnauthorizedError,
+} from '../errors/customErrors.js';
 // Utils
 import { JOB_STATUS, JOB_TYPE } from '../utils/constants.js';
 
@@ -17,6 +21,9 @@ const withValidationErrors = (validateValues) => {
         const errorMessages = errors.array().map((error) => error.msg);
         if (errorMessages[0].startsWith('no job')) {
           throw new NotFoundError(errorMessages);
+        }
+        if (errorMessages[0].startsWith('not authorized')) {
+          throw new UnauthorizedError(errorMessages);
         }
         throw new BadRequestError(errorMessages);
       }
@@ -39,12 +46,17 @@ export const validateJobInput = withValidationErrors([
 
 export const validateIdParam = withValidationErrors([
   // Validate id
-  param('id').custom(async (value) => {
+  param('id').custom(async (value, { req }) => {
     const isValid = mongoose.Types.ObjectId.isValid(value);
     if (!isValid) throw new BadRequestError('invalid MongoDB id');
 
     const job = await Job.findById(value);
     if (!job) throw new NotFoundError(`no job with id: ${value}`);
+
+    const isAdmin = req.user.role === 'admin';
+    const isOwner = req.user.userId === job.createdBy.toString();
+    if (!isAdmin && !isOwner)
+      throw new UnauthorizedError('not authorized to access this router');
   }),
 ]);
 
