e868fc2f456978194083fe344edef1ee823036c1
Adriano Ayala <adriano.ayc@gmail.com>
2024-10-30 18:29:35 -0400

    Add User to Job Routes

--- jobify-app/jobify/controllers/jobControllers.js
+++ jobify-app/jobify/controllers/jobControllers.js
@@ -4,13 +4,13 @@ import Job from '../models/JobModel.js';
 
 // Get All Jobs
 export const getAllJobs = async (req, res) => {
-  console.log(req.user);
-  const jobs = await Job.find({});
+  const jobs = await Job.find({ createdBy: req.user.userId });
   res.status(StatusCodes.OK).json({ jobs });
 };
 
 // Create Job
 export const createJob = async (req, res) => {
+  req.body.createdBy = req.user.userId;
   const job = await Job.create(req.body);
   res.status(StatusCodes.CREATED).json({ job });
 };
