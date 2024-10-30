8de1435deae8e7b3027d1cbddb3a299a1f5e4cab
Adriano Ayala <adriano.ayc@gmail.com>
2024-10-29 23:57:39 -0400

    Validate Login

--- jobify-app/jobify/routes/authRouter.js
+++ jobify-app/jobify/routes/authRouter.js
@@ -1,12 +1,15 @@
 import { Router } from 'express';
 // Middlewares
-import { validateRegisterInput } from '../middleware/validationMiddleware.js';
+import {
+  validateRegisterInput,
+  validateLoginInput,
+} from '../middleware/validationMiddleware.js';
 // Controllers
 import { register, login } from '../controllers/authController.js';
 
 const router = Router();
 
 router.post('/register', validateRegisterInput, register);
-router.post('/login', login);
+router.post('/login', validateLoginInput, login);
 
 export default router;
