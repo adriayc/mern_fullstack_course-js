44ec620284458ea8a0be624f8d3edbcd1c265634
Adriano Ayala <adriano.ayc@gmail.com>
2024-11-01 11:58:43 -0400

    Select Input

--- jobify-app/jobify/client/src/pages/AddJob.jsx
+++ jobify-app/jobify/client/src/pages/AddJob.jsx
@@ -1,8 +1,10 @@
 import { Form, useNavigation, useOutletContext } from 'react-router-dom';
 // Wrappers
 import Wrapper from '../assets/wrappers/DashboardFormPage';
+// Utils
+import { JOB_STATUS, JOB_TYPE } from '../../../utils/constants';
 // Components
-import { FormRow } from '../components';
+import { FormRow, FormRowSelect } from '../components';
 
 const AddJob = () => {
   const { user } = useOutletContext();
@@ -22,6 +24,19 @@ const AddJob = () => {
             labelText="job location"
             name="jobLocation"
             defaultValue={user.location}
+          />
+          {/* Select Input */}
+          <FormRowSelect
+            name="jobStatus"
+            labelText="job status"
+            list={Object.values(JOB_STATUS)}
+            defaultValue={JOB_STATUS.PENDING}
+          />
+          <FormRowSelect
+            name="jobType"
+            labelText="job type"
+            list={Object.values(JOB_TYPE)}
+            defaultValue={JOB_TYPE.FULL_TIME}
           />
           <button
             type="submit"
