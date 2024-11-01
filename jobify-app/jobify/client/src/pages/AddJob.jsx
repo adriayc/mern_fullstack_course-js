96a1903844925cc9d2cc0f4797829997165cc608
Adriano Ayala <adriano.ayc@gmail.com>
2024-11-01 11:27:59 -0400

    Add Job Structure

--- jobify-app/jobify/client/src/pages/AddJob.jsx
+++ jobify-app/jobify/client/src/pages/AddJob.jsx
@@ -1,5 +1,39 @@
+import { Form, useNavigation, useOutletContext } from 'react-router-dom';
+// Wrappers
+import Wrapper from '../assets/wrappers/DashboardFormPage';
+// Components
+import { FormRow } from '../components';
+
 const AddJob = () => {
-  return <h1>AddJob Page</h1>;
+  const { user } = useOutletContext();
+
+  const navigation = useNavigation();
+  const isSubmitting = navigation.state === 'submitting';
+
+  return (
+    <Wrapper>
+      <Form method="post" className="form">
+        <h4 className="form-title">add job</h4>
+        <div className="form-center">
+          <FormRow text="text" name="position" />
+          <FormRow text="text" name="company" />
+          <FormRow
+            type="text"
+            labelText="job location"
+            name="jobLocation"
+            defaultValue={user.location}
+          />
+          <button
+            type="submit"
+            className="btn btn-block form-btn"
+            disabled={isSubmitting}
+          >
+            {isSubmitting ? 'Submitting...' : 'Submit'}
+          </button>
+        </div>
+      </Form>
+    </Wrapper>
+  );
 };
 
 export default AddJob;
