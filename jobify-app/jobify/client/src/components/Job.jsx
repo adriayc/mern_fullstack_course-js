8e3a9145a2404399b7fc8c2ce3de6373ba97af5b
Adriano Ayala <adriano.ayc@gmail.com>
2024-11-01 18:52:28 -0400

    Job Component

--- jobify-app/jobify/client/src/components/Job.jsx
+++ jobify-app/jobify/client/src/components/Job.jsx
@@ -1,5 +1,51 @@
-const Job = () => {
-  return <h3>Job</h3>;
+import { FaBriefcase, FaCalendarAlt, FaLocationArrow } from 'react-icons/fa';
+import day from 'dayjs';
+import advancedFormat from 'dayjs/plugin/advancedFormat';
+day.extend(advancedFormat);
+// Wrappers
+import Wrapper from '../assets/wrappers/Job';
+// Components
+import JobInfo from './JobInfo';
+import { Form, Link } from 'react-router-dom';
+
+const Job = ({
+  _id,
+  position,
+  company,
+  jobLocation,
+  jobType,
+  jobStatus,
+  createdAt,
+}) => {
+  const date = day(createdAt).format('MMM Do, YYYY');
+
+  return (
+    <Wrapper>
+      <header>
+        <div className="main-icon">{company.charAt(0)}</div>
+        <div className="info">
+          <h5>{position}</h5>
+          <p>{company}</p>
+        </div>
+      </header>
+      <div className="content">
+        <div className="content-center">
+          <JobInfo icon={<FaLocationArrow />} text={jobLocation} />
+          <JobInfo icon={<FaCalendarAlt />} text={date} />
+          <JobInfo icon={<FaBriefcase />} text={jobType} />
+          <div className={`status ${jobStatus}`}>{jobStatus}</div>
+        </div>
+        <footer className="actions">
+          <Link className="btn edit-btn">Edit</Link>
+          <Form>
+            <button type="submit" className="btn delete-btn">
+              Delete
+            </button>
+          </Form>
+        </footer>
+      </div>
+    </Wrapper>
+  );
 };
 
 export default Job;
