7dce58ed9f235bc3b286e863cd82a3ee81261c85
Adriano Ayala <adriano.ayc@gmail.com>
2024-11-02 16:10:20 -0400

    Profile Action

--- jobify-app/jobify/client/src/App.jsx
+++ jobify-app/jobify/client/src/App.jsx
@@ -20,6 +20,7 @@ import { action as loginAction } from './pages/Login';
 import { action as addJobAction } from './pages/AddJob';
 import { action as editJobAction } from './pages/EditJob';
 import { action as deleteJobAction } from './pages/DeleteJob';
+import { action as profileAction } from './pages/Profile';
 // Loaders
 import { loader as dashboardLoader } from './pages/DashboardLayout';
 import { loader as allJobsLoader } from './pages/AllJobs';
@@ -80,6 +81,7 @@ const router = createBrowserRouter([
           {
             path: 'profile',
             element: <Profile />,
+            action: profileAction,
           },
           {
             path: 'admin',
--- jobify-app/jobify/client/src/pages/Profile.jsx
+++ jobify-app/jobify/client/src/pages/Profile.jsx
@@ -3,6 +3,28 @@ import { Form, useNavigation, useOutletContext } from 'react-router-dom';
 import Wrapper from '../assets/wrappers/DashboardFormPage';
 // Components
 import { FormRow } from '../components';
+import { toast } from 'react-toastify';
+import customFetch from '../utils/customFetch';
+
+// Action
+export const action = async ({ request }) => {
+  const formData = await request.formData();
+
+  const file = formData.get('avatar');
+  // Validate (> 0.5 MB)
+  if (file && file.size > 500000) {
+    toast.error('Image size too large');
+    return null;
+  }
+
+  try {
+    await customFetch.patch('/users/update-user', formData);
+    toast.success('Profile updated successfully');
+  } catch (error) {
+    toast.error(error?.response?.data?.msg);
+  }
+  return null;
+};
 
 const Profile = () => {
   const { user } = useOutletContext();
