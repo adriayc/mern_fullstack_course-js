341547299a6fc6ba236e72b9cc339a4ab9e5c452
Adriano Ayala <adriano.ayc@gmail.com>
2024-11-02 00:19:34 -0400

    Edit Job Loader

--- jobify-app/jobify/client/src/pages/EditJob.jsx
+++ jobify-app/jobify/client/src/pages/EditJob.jsx
@@ -1,11 +1,20 @@
-import { useParams } from 'react-router-dom';
+import { redirect, useLoaderData, useParams } from 'react-router-dom';
+import { toast } from 'react-toastify';
 // Wrappers
 import Wrapper from '../assets/wrappers/DashboardFormPage';
+// Custom fetch (Axios)
+import customFetch from '../utils/customFetch';
 
 // Loader
 export const loader = async ({ params }) => {
-  console.log(params);
-  return null;
+  try {
+    const { data } = await customFetch.get(`/jobs/${params.id}`);
+    return data;
+  } catch (error) {
+    // console.log(error);
+    toast.error(error?.response?.data?.msg);
+    return redirect('/dashboard/all-jobs');
+  }
 };
 
 // Action
@@ -14,8 +23,9 @@ export const action = async () => {
 };
 
 const EditJob = () => {
-  const params = useParams();
-  console.log(params);
+  // const params = useParams();
+  const { job } = useLoaderData();
+  console.log(job);
 
   return <h1>EditJob Page</h1>;
 };
