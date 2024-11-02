fd11b84457478c7442d45cfc98b0116fff38b319
Adriano Ayala <adriano.ayc@gmail.com>
2024-11-02 11:28:53 -0400

    Admin Page Setup

--- jobify-app/jobify/client/src/pages/Admin.jsx
+++ jobify-app/jobify/client/src/pages/Admin.jsx
@@ -1,5 +1,33 @@
+import { redirect, useLoaderData } from 'react-router-dom';
+import { toast } from 'react-toastify';
+// Wrappers
+import Wrapper from '../assets/wrappers/StatsContainer';
+// Custom fetch (Axios)
+import customFetch from '../utils/customFetch';
+
+// Loader
+export const loader = async () => {
+  try {
+    const { data } = await customFetch.get('/users/admin/app-stats');
+    return data;
+  } catch (error) {
+    // console.log(error);
+    // toast.error(error?.response?.data?.msg);
+    // return error;
+    toast.error('You are not authorized to view this page');
+    return redirect('/dashboard');
+  }
+};
+
 const Admin = () => {
-  return <h1>Admin Page</h1>;
+  const data = useLoaderData();
+  console.log(data);
+
+  return (
+    <Wrapper>
+      <h1>admin page</h1>
+    </Wrapper>
+  );
 };
 
 export default Admin;
