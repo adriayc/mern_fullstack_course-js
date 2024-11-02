93c3622ce2be2be6ba455f0275e833e4323befc6
Adriano Ayala <adriano.ayc@gmail.com>
2024-11-02 15:54:37 -0400

    Profile Page Structure

--- jobify-app/jobify/client/src/pages/Profile.jsx
+++ jobify-app/jobify/client/src/pages/Profile.jsx
@@ -1,5 +1,55 @@
+import { Form, useNavigation, useOutletContext } from 'react-router-dom';
+// Wrappers
+import Wrapper from '../assets/wrappers/DashboardFormPage';
+// Components
+import { FormRow } from '../components';
+
 const Profile = () => {
-  return <h1>Profile Page</h1>;
+  const { user } = useOutletContext();
+  const { name, lastName, email, location } = user;
+
+  const navigation = useNavigation();
+  const isSubmitting = navigation.state === 'submitting';
+
+  return (
+    <Wrapper>
+      {/* encType is super important (upload file) */}
+      <Form method="post" className="form" encType="multipart/form-data">
+        <h4 className="form-title">profile</h4>
+        <div className="form-center">
+          {/* File input */}
+          <div className="form-row">
+            <label htmlFor="avatar" className="form-label">
+              Select an image file (max 0.5 MB)
+            </label>
+            <input
+              type="file"
+              name="avatar"
+              id="avatar"
+              className="form-input"
+              accept="image/*"
+            />
+          </div>
+          <FormRow type="text" name="name" defaultValue={name} />
+          <FormRow
+            type="text"
+            name="lastName"
+            labelText="last name"
+            defaultValue={lastName}
+          />
+          <FormRow type="email" name="email" defaultValue={email} />
+          <FormRow type="text" name="location" defaultValue={location} />
+          <button
+            type="submit"
+            className="btn btn-block form-btn"
+            disabled={isSubmitting}
+          >
+            {isSubmitting ? 'Submitting...' : 'Submit'}
+          </button>
+        </div>
+      </Form>
+    </Wrapper>
+  );
 };
 
 export default Profile;
