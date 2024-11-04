4ebfc25f65e6a0e9499bdff487b27ad0feb44b95
Adriano Ayala <adriano.ayc@gmail.com>
2024-11-04 16:01:43 -0500

    Search Param

--- jobify-app/jobify/controllers/jobControllers.js
+++ jobify-app/jobify/controllers/jobControllers.js
@@ -6,11 +6,21 @@ import Job from '../models/JobModel.js';
 
 // Get All Jobs
 export const getAllJobs = async (req, res) => {
-  console.log(req.query);
-  const jobs = await Job.find({
+  const { search } = req.query;
+
+  const queryObject = {
     createdBy: req.user.userId,
-    position: req.query.search,
-  });
+  };
+
+  // Search params
+  if (search) {
+    queryObject.$or = [
+      { position: { $regex: search, $options: 'i' } },
+      { company: { $regex: search, $options: 'i' } },
+    ];
+  }
+
+  const jobs = await Job.find(queryObject);
   res.status(StatusCodes.OK).json({ jobs });
 };
 
