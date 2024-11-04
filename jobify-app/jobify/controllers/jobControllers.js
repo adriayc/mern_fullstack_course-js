070f83f44a3f170198fde6c1404f5adf12e2b146
Adriano Ayala <adriano.ayc@gmail.com>
2024-11-03 23:23:25 -0500

    Group by Monthly Applications

--- jobify-app/jobify/controllers/jobControllers.js
+++ jobify-app/jobify/controllers/jobControllers.js
@@ -63,20 +63,42 @@ export const showStats = async (req, res) => {
     declined: stats.declined || 0,
   };
 
-  let monthlyApplications = [
+  let monthlyApplications = await Job.aggregate([
     {
-      date: 'Aug 23',
-      count: 12,
+      $match: {
+        createdBy: mongoose.Types.ObjectId.createFromHexString(req.user.userId),
+      },
     },
     {
-      date: 'Sep 23',
-      count: 9,
+      $group: {
+        _id: {
+          year: { $year: '$createdAt' },
+          month: { $month: '$createdAt' },
+        },
+        count: { $sum: 1 },
+      },
     },
     {
-      date: 'Oct 23',
-      count: 3,
+      $sort: { '_id.year': -1, '_id.month': -1 },
     },
-  ];
+    {
+      $limit: 6,
+    },
+  ]);
+  monthlyApplications = monthlyApplications
+    .map((item) => {
+      const {
+        _id: { year, month },
+        count,
+      } = item;
+      const date = day()
+        .month(month - 1)
+        .year(year)
+        .format('MMM YY');
+      return { date, count };
+    })
+    .reverse();
+  // console.log(monthlyApplications);
 
   res.status(StatusCodes.OK).json({ defaultStats, monthlyApplications });
 };
