6e7435387f85b185b33e4864331acec26ea6f0d2
Adriano Ayala <adriano.ayc@gmail.com>
2024-11-04 16:37:30 -0500

    Pagination

--- jobify-app/jobify/controllers/jobControllers.js
+++ jobify-app/jobify/controllers/jobControllers.js
@@ -36,8 +36,22 @@ export const getAllJobs = async (req, res) => {
   };
   const sortKey = sortOptions[sort] || sortOptions.newest;
 
-  const jobs = await Job.find(queryObject).sort(sortKey);
-  res.status(StatusCodes.OK).json({ jobs });
+  // Setup pagination
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
+  const skip = (page - 1) * limit;
+
+  const jobs = await Job.find(queryObject)
+    .sort(sortKey)
+    .skip(skip)
+    .limit(limit);
+  // Total jobs
+  const totalJobs = await Job.countDocuments(queryObject);
+  // Number of pages
+  const numOfPages = Math.ceil(totalJobs / limit);
+  res
+    .status(StatusCodes.OK)
+    .json({ totalJobs, numOfPages, currentPage: page, jobs });
 };
 
 // Create Job
