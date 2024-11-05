3b1e901617ef7506ae72603a053aa93f2b1636e3
Adriano Ayala <adriano.ayc@gmail.com>
2024-11-05 15:26:10 -0400

    Button Container CSS (optional)

--- jobify-app/jobify/client/src/assets/wrappers/PageBtnContainer.js
+++ jobify-app/jobify/client/src/assets/wrappers/PageBtnContainer.js
@@ -14,46 +14,47 @@ const Wrapper = styled.section`
     display: flex;
   }
   .page-btn {
-    background: transparent;
-    border-color: transparent;
     width: 50px;
     height: 40px;
-    font-weight: 700;
-    font-size: 1.25rem;
     color: var(--primary-500);
-    border-radius: var(--border-radius);
-    cursor:pointer:
-  }
-  .active{
-    background:var(--primary-500);
-        color: var(--white);
-
-  }
-  .prev-btn,.next-btn{
-    background: var(--background-secondary-color);
+    background: transparent;
+    font-size: 1.25rem;
+    font-weight: 700;
     border-color: transparent;
-        border-radius: var(--border-radius);
-
+    border-radius: var(--border-radius);
+    cursor: pointer;
+  }
+  .active {
+    color: var(--white);
+    background: var(--primary-500);
+  }
+  .prev-btn,
+  .next-btn {
     width: 100px;
     height: 40px;
-        color: var(--primary-500);
-text-transform:capitalize;
-letter-spacing:var(--letter-spacing);
-display:flex;
-align-items:center;
-justify-content:center;
-gap:0.5rem;
-cursor:pointer;
+    color: var(--primary-500);
+    background: var(--background-secondary-color);
+    text-transform: capitalize;
+    letter-spacing: var(--letter-spacing);
+    border-color: transparent;
+    border-radius: var(--border-radius);
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 0.5rem;
+    cursor: pointer;
   }
-  .prev-btn:hover,.next-btn:hover{
-    background:var(--primary-500);
-        color: var(--white);
-        transition:var(--transition);
+  .prev-btn:hover,
+  .next-btn:hover {
+    color: var(--white);
+    background: var(--primary-500);
+    transition: var(--transition);
   }
-.dots{
-  display:grid;
-  place-items:center;
-  cursor:text;
-}
+  .dots {
+    cursor: pointer;
+    display: grid;
+    place-items: center;
+  }
 `;
+
 export default Wrapper;
