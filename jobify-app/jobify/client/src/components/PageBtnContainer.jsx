7bdcdfd428b82571b527b08ae99497588daf32aa
Adriano Ayala <adriano.ayc@gmail.com>
2024-11-05 11:12:53 -0400

    Render Buttons

--- jobify-app/jobify/client/src/components/PageBtnContainer.jsx
+++ jobify-app/jobify/client/src/components/PageBtnContainer.jsx
@@ -1,3 +1,4 @@
+import { HiChevronDoubleLeft, HiChevronDoubleRight } from 'react-icons/hi';
 // Wrappers
 import Wrapper from '../assets/wrappers/PageBtnContainer';
 // Custom hook (Context)
@@ -7,9 +8,40 @@ const PageBtnContainer = () => {
   const {
     data: { numOfPages, currentPage },
   } = useAllJobsContext();
-  console.log(numOfPages, currentPage);
 
-  return <h1>PageBtnContainer</h1>;
+  // Pages
+  const pages = Array.from({ length: numOfPages }, (_, index) => {
+    return index + 1;
+  });
+  // console.log(pages);
+
+  return (
+    <Wrapper>
+      <button type="button" className="btn prev-btn">
+        <HiChevronDoubleLeft />
+        prev
+      </button>
+      <div className="btn-container">
+        {pages.map((pageNumber) => {
+          return (
+            <button
+              key={pageNumber}
+              type="button"
+              className={`btn page-btn ${
+                pageNumber === currentPage && 'active'
+              }`}
+            >
+              {pageNumber}
+            </button>
+          );
+        })}
+      </div>
+      <button type="button" className="btn next-btn">
+        next
+        <HiChevronDoubleRight />
+      </button>
+    </Wrapper>
+  );
 };
 
 export default PageBtnContainer;
