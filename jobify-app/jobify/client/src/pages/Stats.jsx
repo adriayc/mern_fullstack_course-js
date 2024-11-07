cdc76f10c27f15757429e25ace0cd12be3bb2445
Adriano Ayala <adriano.ayc@gmail.com>
2024-11-07 00:41:54 -0400

    First Query with React Query

--- jobify-app/jobify/client/src/pages/Stats.jsx
+++ jobify-app/jobify/client/src/pages/Stats.jsx
@@ -1,5 +1,6 @@
 import { useLoaderData } from 'react-router-dom';
 import { toast } from 'react-toastify';
+import { useQueries, useQuery } from '@tanstack/react-query';
 // Custom fetch (Axios)
 import customFetch from '../utils/customFetch';
 // Components
@@ -7,22 +8,26 @@ import { ChartsContainer, StatsContainer } from '../components';
 
 // Loader
 export const loader = async () => {
-  // try {
-  //   const { data } = await customFetch.get('/jobs/stats');
-  //   return data;
-  // } catch (error) {
-  //   // console.log(error);
-  //   toast.error(error?.response?.data?.msg);
-  //   return error;
-  // }
-
+  return null;
   const { data } = await customFetch.get('/jobs/stats');
   return data;
 };
 
 const Stats = () => {
-  const { defaultStats, monthlyApplications } = useLoaderData();
-  // console.log(monthlyApplications);
+  // const { defaultStats, monthlyApplications } = useLoaderData();
+
+  // const response = useQuery({
+  const { isLoading, isError, data } = useQuery({
+    queryKey: ['stats'],
+    queryFn: () => customFetch.get('/jobs/stats'),
+  });
+  // console.log(data);
+
+  if (isLoading) return <h4>Loading...</h4>;
+  if (isError) return <h4>Error...</h4>;
+  const { defaultStats, monthlyApplications } = data.data;
+
+  // return <h1>React query</h1>;
 
   return (
     <>
